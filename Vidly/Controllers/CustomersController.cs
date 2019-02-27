8ec0298c0cb33795f63696f67e1bd3a3c50f9354
Howard <a26421965@hotmail.com.tw>
2019-02-27 20:48:36 +0800

    增加客戶新增和修改功能

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -42,6 +43,64 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             }
             return View(myCustomer);
+        }
+
+        public ActionResult New()
+        {
+            var myMembershiptype = _context.MembershipTypes.ToList();
+
+            //當需要給View多個Model時則先建立ViewModel
+            var myNewCustomerViewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = myMembershiptype
+            };
+
+            return View(myNewCustomerViewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("New",viewModel);
+        }
+
+        /// <summary>
+        /// 雖然前端是一個ViewModel，但這個ViewModel中包含Customer,所以這邊用Customer當參數接收HttpPost
+        /// </summary>
+        /// <param name="Customer">重點!!! 這個客戶參數的名稱要與ViewModel的客戶完全相同</param>
+        /// <returns></returns>
+        [HttpPost]//限制這個function只能用HttpPost呼叫
+        public ActionResult Save(Customer Customer)
+        {
+            //看放在前端的HiddenFor(Id)來決定要做新增還是修改動作
+            if (Customer.Id==0)
+            {
+                _context.Customers.Add(Customer);//新增                
+            }
+            else
+            {
+                var myCustomer = _context.Customers.Single(c => c.Id == Customer.Id);
+                myCustomer.Name = Customer.Name;
+                myCustomer.Birthdate = Customer.Birthdate;
+                myCustomer.MembershipTypeId = Customer.MembershipTypeId;
+                myCustomer.IsSubscribedToNewsletter = Customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();//要commit才會執行
+
+            return RedirectToAction("Index","Customers"); //(AnctionName,ControllerName)
         }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,20 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
+        [Display(Name = "名稱")]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+       
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "會員類別")]
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "生日")]
         public DateTime? Birthdate { get; set; }
     }
 }
